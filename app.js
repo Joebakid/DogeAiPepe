dc89d3e679921419fae7e6e335aee232602822d5
joebakid <josephbawo@gmail.com>
2023-12-29 19:08:14 +0100

    timer count down added

--- app.js
+++ app.js
@@ -149,3 +149,32 @@ timeln.to(".card-3", {
 });
 
 timeln.to(".card-3", {});
+
+// TIMER
+
+const countDownDate = new Date("30 Dec ,2023 15:00:00").getTime();
+const x = setInterval(function () {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+  // document.getElementById("days").innerHTML = days;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = "00";
+    document.getElementById("hours").innerHTML = "00";
+    document.getElementById("minutes").innerHTML = "00";
+    document.getElementById("seconds").innerHTML = "00";
+  }
+});
+
+console.log(countDownDate);
